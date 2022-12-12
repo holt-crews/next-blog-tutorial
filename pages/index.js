afe0aaad4710e6f0442d670c6019dcdf5c753d0e
Holt Crews <holtcrews47@gmail.com>
2022-12-12 09:54:18 -0500

    Pre-rendering and Data fetching section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,37 @@
 import Head from "next/head";
-import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+// this makes it use static generation. HTML is generated at build time instead of request time
+// this function name is special and always gets put into the "Home" props
+// getStaticProps only runs on the server-side.
+// It will never run on the client-side.
+// It won’t even be included in the JS bundle for the browser.
+// That means you can write code such as direct database queries without them being sent to browsers.
+// getStaticProps can only be exported from a page. You can’t export it from non-page files.
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+// this function gets executed at request time
+// this seems pretty expensive on the server side, feels relatively limited.
+// I would think you can usually just do client-side fetching
+// only necessary if you need to pre-render data
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       // props for your component
+//     },
+//   };
+// }
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -16,9 +44,20 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
-      <h2 className={utilStyles.headingLg}>
-        <Link href="/posts/first-post">Check out this post</Link>
-      </h2>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 }
